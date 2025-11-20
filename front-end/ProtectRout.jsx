8420107010e9eb9a748c65243c5897de87397fb6
robsonldcarvalho <143975321+robsonldcarvalho@users.noreply.github.com>
2025-11-20 12:59:47 -0300

    Reestruturação da página login

--- front-end/ProtectRout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-
-export default function ProtectedRoute({ children }) {
-const user = localStorage.getItem('rentabil_user');
-if (!user) return <Navigate to="/" replace />;
-return children;
-}
