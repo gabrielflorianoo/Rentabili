07820c09463e7e0b77513a322694a58fc8a43b99
Wilson Santos <pw.druida@gmail.com>
2025-11-24 09:25:48 -0300

    fix: tailwind e chart.js

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,7 +1,9 @@
-import tailwindcss from "@tailwindcss/postcss";
+import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
   plugins: [
-    tailwindcss(),
-  ],
-};
+    tailwind(),
+    autoprefixer(),
+  ]
+}
