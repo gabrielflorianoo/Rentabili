4521a0345552f2350340c0e7092994c86b476eb9
robsonldcarvalho <143975321+robsonldcarvalho@users.noreply.github.com>
2025-12-01 06:56:22 -0300

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
                         <path d="M18.7 8l-5.1 5.2-2.8-2.7L7 14.3" />
                     </svg>
                 </div>
-                <span className="logo-text">RENTABIL</span>
+                <span className="logo-text">RENTABILI</span>
             </div>
 
             {/* Menu de navegação */}
